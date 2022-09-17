029b492a90a6ccc460e4dbd62d78b2d6d3a6114d
dinithpv <113197398+dinithpv@users.noreply.github.com>
2022-09-17 09:08:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -17,12 +17,12 @@ global.APIKeys = {
 
 //AUTO FUNCTIONS ( 📵 DONT EDIT 📵 ) ITS FOR NEXT UPDATE
 
-global.autoTyping = true //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc =flase //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = false //auto reading in gc (true to on, false to off)
 global.autobio = false  // auto bio system checking on Developing Leval
 global.autoRecord = false //auto recording (true to on, false to off)
-global.available = true //auto available (true to on, false to off)
+global.available = false //auto available (true to on, false to off)
 
 //VIDEO MENU 
 global.vidmenu = fs.readFileSync("./LocalData/theme/cyber.mp4") //gif and video menu
